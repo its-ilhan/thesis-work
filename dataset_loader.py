f8b9c03b178730ad19242e2e9bcf8b2c32e75cb7
its-ilhan <ilhanhasan2027@gmail.com>
2026-03-18 23:00:22 +0600

    10000 to 14000

--- dataset_loader.py
+++ dataset_loader.py
@@ -121,10 +121,10 @@ def build_dataset(whisper_model_size: str = WHISPER_MODEL) -> pd.DataFrame:
     # all_files = real_files + fake_files
     # print(f"Full dataset: {len(real_files)} real + {len(fake_files)} fake files.\n")
 
-    CAP = 9000  # adjust this number based on available time
+    CAP = 14000  # adjust this number based on available time
 
-    real_files = real_files[5000:CAP]
-    fake_files = fake_files[5000:CAP]
+    real_files = real_files[10000:CAP]
+    fake_files = fake_files[10000:CAP]
     all_files  = real_files + fake_files
 
     print(f"Training run: {len(real_files)} real + {len(fake_files)} fake files.")
