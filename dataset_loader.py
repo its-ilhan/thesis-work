1ae55e4fc772b7f83cce6f2bbf3d5a87457347ac
its-ilhan <ilhanhasan2027@gmail.com>
2026-03-30 16:18:18 +0600

    37000 to 40000

--- dataset_loader.py
+++ dataset_loader.py
@@ -127,9 +127,9 @@ def build_dataset(whisper_model_size: str = WHISPER_MODEL) -> pd.DataFrame:
     rng.shuffle(real_files)
     rng.shuffle(fake_files)
 
-    CAP = 30000
-    real_files = real_files[25000:CAP]
-    fake_files = fake_files[25000:CAP]
+    CAP = 40000
+    real_files = real_files[37000:CAP]
+    fake_files = fake_files[37000:CAP]
     all_files  = real_files + fake_files
 
     print(f"Training run: {len(real_files)} real + {len(fake_files)} fake files.")
